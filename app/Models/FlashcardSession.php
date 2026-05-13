3508813585de1c22bd76a466554ec79d37374907
AngelFFA <affloresa@alumno.uspg.edu.gt>
2026-05-13 11:39:12 -0600

    feat: crear modelo y migración de flashcard_sessions

---
+++ app/Models/FlashcardSession.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FlashcardSession extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'topic',
+        'target_language',
+    ];
+
+    //una sesión pertenece a un usuario.
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //Una sesión tiene muchas flashcards.
+    public function flashcards()
+    {
+        return $this->hasMany(Flashcard::class, 'session_id');
+    }
+}
