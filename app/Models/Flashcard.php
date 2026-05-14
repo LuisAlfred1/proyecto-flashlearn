52269a3c104ed14de62b78e609f6d9702f130b09
Luis Reyes <alfredluis177@gmail.com>
2026-05-13 20:40:20 -0600

    Se implemento la lógica en la pagina de mis-flashcards

--- app/Models/Flashcard.php
+++ app/Models/Flashcard.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $flashcard_session_id
+ * @property int $session_id
+ * @property string $topic
+ * @property string $target_language
+ * @property string $original_word
+ * @property string $translated_word
+ * @property string $example_sentence
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property-read \App\Models\FlashcardSession $flashcardSession
+ */
 class Flashcard extends Model
 {
     use HasFactory;
@@ -20,7 +34,7 @@ class Flashcard extends Model
     ];
 
     //Una flashcard pertenece a una sesión.
-    public function flashcardSession()
+    public function flashcardSession(): BelongsTo
     {
         return $this->belongsTo(FlashcardSession::class, 'flashcard_session_id');
     }
